924bb8cd32198b837e217c08e0477c5a5f428b9d
Rajkalwar <85445769+Rajkalwar@users.noreply.github.com>
2022-06-29 16:16:30 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,18 @@
+<?php
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+$email_from='1905928@kiit.ac.in';
+$email_subject="New Form Submission";
+$email_body="User Name: $name.\n".
+"User Email: $email.\n".
+"User Subject: $subject.\n".
+"User Message: $message.\n";
+$to="guptarajbirta@gmail.com";
+$headers="From: $email_from \r\n";
+$headers.="Reply-To: $email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+
+?>
